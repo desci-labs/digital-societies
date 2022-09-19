2c60362abea227eee60bb7280882d51cfb386477
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-09-19 19:21:51 +0100

    declare pinata env in .next.config file

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     domains: ["ipfs.io", "gateway.pinata.cloud", "w3s.link"],
   },
+  env: {
+    PINATA_API_KEY: process.env.PINATA_API_KEY,
+    PINATA_SECRET_KEY: process.env.PINATA_SECRET_KEY,
+  },
 };
 
 module.exports = nextConfig;
