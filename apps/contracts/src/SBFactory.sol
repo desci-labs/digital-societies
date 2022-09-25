fd29ab4e07540fbb9c59ee6744c3b3d3ce1f0d86
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-09-25 04:35:42 +0100

    document all contracts using the solidity natspec comments

--- apps/contracts/src/SBFactory.sol
+++ apps/contracts/src/SBFactory.sol
@@ -2,9 +2,21 @@
 pragma solidity 0.8.13;
 import "src/SBToken.sol";
 
+
+/// @title An experimental implementation of a soul-bound token (SBT) Factory smart contract
+/// @author Oloyede Shadrach Temitayo (@oloyedeshadrach)
+/// @notice You can use this contract to deploy a SBT contract for your organisation
+/// @dev All functions are subject to changes in the future.
+/// @custom:experimental This is an experimental contract for DeSci Labs (https://desci.com).
 contract SBFactory {
     event TokenCreated(address indexed token, address indexed owner);
 
+
+    /// @notice Deploy a new SBT contract to be associate to the Desci Labs factory contract
+    /// @dev creates a new SBToken contract using the create2 opcode
+    /// @param _name the name of the SBToken contract (passed to the constructor)
+    /// @param _symbol the symbol of the SBToken contract (passed to the constructor)
+    /// @param _metadata the cid of the metadata to be associated to the deployed SBToken contract
     function deployToken(
         string memory _name,
         string memory _symbol,
--- apps/contracts/src/SBToken.sol
+++ apps/contracts/src/SBToken.sol
@@ -4,6 +4,11 @@ pragma solidity 0.8.13;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
+/// @title An experimental implementation of a soul-bound token (SBT) smart contract
+/// @author Oloyede Shadrach Temitayo (@oloyedeshadrach)
+/// @notice You can use this contract to issue soul-bound credentials to users or other smart contracts
+/// @dev All functions are subject to changes in the future.
+/// @custom:experimental This is an experimental contract.
 contract SBToken is ERC721, AccessControlEnumerable {
     uint256 public totalSupply;
     uint16 public totalTypes;
@@ -17,8 +22,16 @@ contract SBToken is ERC721, AccessControlEnumerable {
     mapping(uint256 => uint16) public tokenIdToType;
     mapping(uint16 => mapping(address => bool)) public typeToSoul;
 
-    event Revoked(address indexed revokedBy, address indexed owner, uint256 tokenId);
-    event BatchRevoked(address indexed revokedBy, address[] owners, uint256[] tokenIds);
+    event Revoked(
+        address indexed revokedBy,
+        address indexed owner,
+        uint256 tokenId
+    );
+    event BatchRevoked(
+        address indexed revokedBy,
+        address[] owners,
+        uint256[] tokenIds
+    );
     event Mint(
         address indexed mintedBy,
         address indexed to,
@@ -42,17 +55,10 @@ contract SBToken is ERC721, AccessControlEnumerable {
         _grantRole(DEFAULT_ADMIN_ROLE, _admin);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(AccessControlEnumerable, ERC721)
-        returns (bool)
-    {
-        return
-            interfaceId == type(IAccessControlEnumerable).interfaceId ||
-            super.supportsInterface(interfaceId);
-    }
-
+    /// @notice Mint a new credential type(SBT) for this organisation
+    /// @dev Mints a new token type that can be issued to users
+    /// @dev The new type is linked to a new ipfs hash linked to it's metadata
+    /// @param typeURI_ is the uri (ipfs hash) of the metadata associated to this mint
     function mintTokenType(bytes calldata typeURI_)
         external
         onlyRole(DELEGATE_ROLE)
@@ -63,36 +69,17 @@ contract SBToken is ERC721, AccessControlEnumerable {
         emit TypeCreated(totalTypes, msg.sender, typeURI_);
     }
 
-    //@notice Mints a new token to the given address,
-    // can only be called by admins of this SBT or the admin of the contract
-    function mint(address _to, uint16 _tokenType)
-        public
-        onlyRole(DELEGATE_ROLE)
-    {
-        require(_typeExists(_tokenType), "Invalid SB type");
-        require(!_hasType(_to, _tokenType), "Duplicate credential");
-        totalSupply++;
-        _safeMint(_to, totalSupply);
-        tokenToMinter[totalSupply] = msg.sender;
-        tokenIdToType[totalSupply] = _tokenType;
-        typeToSoul[_tokenType][_to] = true;
-        emit Mint(msg.sender, _to, totalSupply, _tokenType);
-    }
-
+    /// @notice Issue a credential (SBT) to multiple users in a single call
+    /// @dev Mints a new token or issue a token type to multiple wallet addresses
+    /// @param _to an array of address to receive minted tokens
+    /// @param _tokenType token type to be issued for this mint
     function batchMint(address[] memory _to, uint16 _tokenType)
-        public
+        external
         onlyRole(DELEGATE_ROLE)
     {
         require(_typeExists(_tokenType), "Invalid SB type");
         for (uint256 i = 0; i < _to.length; ) {
-            // TODO:  mint(_to[i], _tokenType);
-            require(!_hasType(_to[i], _tokenType), "Duplicate credential");
-            totalSupply++;
-            _safeMint(_to[i], totalSupply);
-            tokenToMinter[totalSupply] = msg.sender;
-            tokenIdToType[totalSupply] = _tokenType;
-            typeToSoul[_tokenType][_to[i]] = true;
-            emit Mint(msg.sender, _to[i], totalSupply, _tokenType);
+            mint(_to[i], _tokenType);
 
             unchecked {
                 i++;
@@ -100,12 +87,12 @@ contract SBToken is ERC721, AccessControlEnumerable {
         }
     }
 
-    function burn(uint256 _tokenId) public {
+    /// @notice Allow credential holders to destroy their tokens and revoke their ownership
+    /// @dev This function allows wallet address to burn their tokens
+    /// @param _tokenId Id of the token to be burnt
+    function burn(uint256 _tokenId) external {
         address owner = ownerOf(_tokenId);
-        require(
-            owner == msg.sender,
-            "Only the owner can burn their token"
-        );
+        require(owner == msg.sender, "Only the owner can burn their token");
         _burn(_tokenId);
         tokenToMinter[_tokenId] = address(0);
         typeToSoul[tokenIdToType[_tokenId]][msg.sender] = false;
@@ -113,6 +100,9 @@ contract SBToken is ERC721, AccessControlEnumerable {
         emit Revoked(msg.sender, owner, _tokenId);
     }
 
+    /// @notice This function allows the admin or delegates to revoke a user's credential or revoke their ownership
+    /// @dev Admin and delegate can revoke ownership of token
+    /// @param _tokenId Id of the token to be revoked
     function revoke(uint256 _tokenId) external onlyRole(DELEGATE_ROLE) {
         address owner = ownerOf(_tokenId);
         _burn(_tokenId);
@@ -122,6 +112,9 @@ contract SBToken is ERC721, AccessControlEnumerable {
         emit Revoked(msg.sender, owner, _tokenId);
     }
 
+    /// @notice This function allows the admin or delegates to revoke a multiple user's credential
+    /// @dev Admin and delegate can revoke ownership of tokens in a single transaction
+    /// @param _tokenIds an array of Ids of the tokens to be revoked
     function batchRevoke(uint256[] memory _tokenIds)
         external
         onlyRole(DELEGATE_ROLE)
@@ -142,42 +135,10 @@ contract SBToken is ERC721, AccessControlEnumerable {
         emit BatchRevoked(msg.sender, owners, _tokenIds);
     }
 
-    //@notice Function to fetch the metadata of a token
-    function tokenCID(uint256 _tokenId) public view returns (bytes memory) {
-        return typeToURI[tokenIdToType[_tokenId]];
-    }
-
-    //@notice Function to fetch the metadata of a token
-    function typeURI(uint16 _type) public view returns (bytes memory) {
-        return typeToURI[_type];
-    }
-
-    //@notice Function to add or update metadata of a token
-    function updateTokenIdType(uint256 _tokenId, uint16 _tokenType)
-        external
-        onlyRole(DELEGATE_ROLE)
-    {
-        require(_typeExists(_tokenType), "Invalid SB type");
-        require(_exists(_tokenId), "FORBIDDEN: Invalid tokenId");
-        
-        tokenIdToType[_tokenId] = _tokenType;
-        emit TokenIdTypeUpdated(_tokenId, _tokenType);
-    }
-
-    //@notice Function to fetch the metadata of a token
-    function contractURI() public view returns (bytes memory) {
-        return _contractURI;
-    }
-
-    //@notice Function to add or update contract metadata
-    function setContractURI(bytes calldata contractURI_)
-        public
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        _contractURI = contractURI_;
-    }
-
-    //@notice Function to add or update metadata of a SBT type
+    /// @notice Only admin can update a credential's data
+    /// @dev DEFAULT_ADMIN_ROLE can update the ipfs of a token type
+    /// @param _tokenType token type to update
+    /// @param _typeURI_ new ipfs hash or uri to be set for _tokenType
     function updateTypeURI(uint16 _tokenType, bytes calldata _typeURI_)
         external
         onlyRole(DEFAULT_ADMIN_ROLE)
@@ -186,6 +147,83 @@ contract SBToken is ERC721, AccessControlEnumerable {
         require(_typeURI_.length > 0, "Invalid typeURI");
         typeToURI[_tokenType] = _typeURI_;
         emit TypeUpdated(_tokenType, _typeURI_);
+    }
+
+    /// @notice Admin or Delegates can re-assign a new credential to a user
+    /// @dev Update the token type assigned to a tokenId
+    /// @param _tokenId token Id to be updated
+    /// @param _tokenType new token type to be assigned
+    function updateTokenIdType(uint256 _tokenId, uint16 _tokenType)
+        external
+        onlyRole(DELEGATE_ROLE)
+    {
+        require(_typeExists(_tokenType), "Invalid SB type");
+        require(_exists(_tokenId), "FORBIDDEN: Invalid tokenId");
+
+        tokenIdToType[_tokenId] = _tokenType;
+        emit TokenIdTypeUpdated(_tokenId, _tokenType);
+    }
+
+    /// @notice set a new metadata uri for this contract (organisation)
+    /// @dev update the metadata uri for this contract
+    /// @param contractURI_ ipfs hash or URI of the new metadata
+    function setContractURI(bytes calldata contractURI_)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        _contractURI = contractURI_;
+    }
+
+    /// @notice Return the content identify for user's credential
+    /// @dev Returns the type uri of the input tokenId's credential or type
+    /// @param _tokenId token id to get type cid
+    /// @return ipfs hash of the token type associated to the input tokenId
+    function tokenCID(uint256 _tokenId) external view returns (bytes memory) {
+        return typeToURI[tokenIdToType[_tokenId]];
+    }
+
+    /// @notice Issue a credential (SBT) to a user
+    /// @dev Mints a new token or issue a token type to a wallet addresses
+    /// @param _to address to receive minted tokens
+    /// @param _tokenType token type to be issued for this mint
+    function mint(address _to, uint16 _tokenType)
+        public
+        onlyRole(DELEGATE_ROLE)
+    {
+        require(_typeExists(_tokenType), "Invalid SB type");
+        require(!_hasType(_to, _tokenType), "Duplicate credential");
+        totalSupply++;
+        _safeMint(_to, totalSupply);
+        tokenToMinter[totalSupply] = msg.sender;
+        tokenIdToType[totalSupply] = _tokenType;
+        typeToSoul[_tokenType][_to] = true;
+        emit Mint(msg.sender, _to, totalSupply, _tokenType);
+    }
+
+    /// @notice Return the content identify for a credential
+    /// @dev Returns the type uri of the input credential or sbt type
+    /// @param _type token type ID to get type cid
+    /// @return ipfs cid of the token type
+    function typeURI(uint16 _type) public view returns (bytes memory) {
+        return typeToURI[_type];
+    }
+
+    /// @notice Return the content identify for user's credential
+    /// @dev Returns the cid of the contract metatdata stored on ipfs
+    /// @return returns _contractURI of this SBT contract
+    function contractURI() public view returns (bytes memory) {
+        return _contractURI;
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(AccessControlEnumerable, ERC721)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IAccessControlEnumerable).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 
     //@notice a function that gets called before any token is transferred. Forces the owner to only be able to revoke the token.
@@ -204,7 +242,11 @@ contract SBToken is ERC721, AccessControlEnumerable {
         exists = _type > 0 && _type < totalTypes + 1;
     }
 
-    function _hasType(address owner, uint16 tokenType) internal view returns (bool) {
+    function _hasType(address owner, uint16 tokenType)
+        internal
+        view
+        returns (bool)
+    {
         return typeToSoul[tokenType][owner] == true;
     }
 }
