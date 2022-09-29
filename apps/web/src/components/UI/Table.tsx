0e50de782fa0a3fe3219a8c5d2556d02f613c6d7
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-09-29 15:26:49 +0100

    update table styling

--- apps/web/src/components/UI/Table.tsx
+++ apps/web/src/components/UI/Table.tsx
@@ -12,7 +12,7 @@ export function Table(props: HTMLProps<HTMLTableElement>) {
 export function THead(props: { rows: string[], className?: string }) {
   return (
     <thead className={props.className}>
-      <tr className="bg-neutrals-gray-7 dark:bg-primary dark:bg-opacity-10 text-tint-primary text-left tracking-wide">
+      <tr className="bg-tint-primary dark:bg-opacity-10 text-white dark:text-tint-primary text-left tracking-wide">
         {props.rows.map((cell, i) => <Cell key={i} className="px-2 py-1 font-semibold text-sm font-lg capitalize">{cell}</Cell>)}
       </tr>
     </thead>
@@ -29,7 +29,7 @@ export function TBody(props: HTMLProps<HTMLTableSectionElement>) {
 
 export function Row(props: HTMLProps<HTMLTableRowElement>) {
   return (
-    <tr {...props} className={`border-b border-neutrals-gray-5 hover:bg-neutrals-gray-3 table-row text-md text-left font-normal ${props.className ?? ''}`}>
+    <tr {...props} className={`border-b border-neutrals-gray-5 hover:bg-tint-primary table-row text-md text-left font-normal ${props.className ?? ''}`}>
       {props.children}
     </tr>
   )
