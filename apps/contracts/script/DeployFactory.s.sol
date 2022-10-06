446d1b98183b0b1d5b0702e4e10e05d04904270d
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-10-06 04:34:01 +0100

    refactor scripts

--- apps/contracts/script/DeployFactory.s.sol
+++ apps/contracts/script/DeployFactory.s.sol
@@ -6,11 +6,13 @@ import "src/DesocManager.sol";
 
 contract FactoryScript is Script {
     // forwarder contract address on goerli
-    address public forwarder = 0x7A95fA73250dc53556d264522150A940d4C50238;
+    // address public forwarder = 0x7A95fA73250dc53556d264522150A940d4C50238;
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        address forwarder = vm.envAddress("FORWARDER");
+        require(forwarder != address(0), "Please set the forwarder contract address in you .env file");
         new DesocManager(forwarder);
         vm.stopBroadcast();
     }
--- apps/contracts/scripts/deploy.ts
+++ apps/contracts/scripts/deploy.ts
@@ -8,11 +8,12 @@ async function main() {
   const chainId = (await net).chainId;
   const signers = await ethers.getSigners();
   const deployer = signers[0];
-  console.log('chainId ', chainId, 'Deployer', deployer.address);
+  
+  console.log('Running script on chain: ', chainId, 'Deployer: ', deployer.address);
 
   if (!deployer) throw new Error('configure accounts for deployment in your hardhat.config.ts file');
 
-  let forwarder, relayerHub;
+  let forwarder: string, relayerHub: string;
   const deployedNetwork = (deployedNetworks[chainId] || [])[0];
 
   if (network.name !== "localhost" || !chainId.toString().match(/1337/)) {
@@ -29,11 +30,10 @@ async function main() {
     console.log('Forwarder ', forwarder);
 
     // sanity check: the build/gsn was created on the currently running node.
-    console.log('Deployer is set, running sanity check on forwarder');
+    console.log('Running sanity check on forwarder');
     if (await ethers.provider.getCode(forwarder).then(code => code.length) === 2) {
       throw new Error('GSN is not running. You may use "yarn node:start && yarn deploy-gsn" to launch Hardhat and GSN.')
     }
-
 
     // validate relayerhub
     relayerHub = deployedNetwork.contracts &&
@@ -47,30 +47,30 @@ async function main() {
     //** Deploy custom paymaster here and connect it to relayer & forwarder
     const Paymaster = await ethers.getContractFactory("DesocPaymaster");
     let paymaster = await Paymaster.deploy();
-    console.log("Deploying paymaster at: ", paymaster.address);
+    console.log("Deploying paymaster....");
     await paymaster.deployed();
     console.log("Paymaster deployed at: ", paymaster.address);
 
-    console.log('set forwarder', forwarder);
+    console.log('running setTrustedForwarder on paymaster', forwarder);
     await paymaster.setTrustedForwarder(forwarder);
 
-    console.log('set relayer', relayerHub);
+    console.log('running setRelayHub on relayer', relayerHub);
     await paymaster.setRelayHub(relayerHub);
     
-    console.log('fund paymaster');
+    console.log('funding paymaster....');
     const tx = await paymaster.deposit({ from: deployer.address, value: utils.parseEther('0.5')});
     await tx.wait();
-    console.log('Paymaster funded');
+    console.log('Paymaster funded ✅', tx.hash);
     
   } else {
-    forwarder = localForwarder;
+    forwarder = localForwarder.address;
   }
 
-
+  //** ENABLE THIS LINES BELOW TO DEPLOY DESCO_MANAGER USING HARDHAT INSTEAD OF FOUNDRY
   // const DesocManager = await ethers.getContractFactory("DesocManager");
   // const manager = await DesocManager.deploy(forwarder);
   // await manager.deployed();
-  // console.log(`Deployed SBFactory at ${manager.address} with forwarder ${forwarder}`);
+  // console.log(`Deployed DesocManager at ${manager.address} with forwarder ${forwarder}`);
 
 }
 
