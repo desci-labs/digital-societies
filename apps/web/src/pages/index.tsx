277406169945e7c7176b6ee33c09b78838e05547
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-11-14 13:48:42 +0100

    refactor index page

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -3,9 +3,9 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="py-0 px-10">
+    <section className="py-0 px-10">
       <DesocList />
-    </div>
+    </section>
   );
 };
 
