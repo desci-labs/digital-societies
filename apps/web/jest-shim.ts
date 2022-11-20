3f93de420d1193ef0c2ad50978cd14ffc5a19d40
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-11-20 20:27:58 +0100

    rebuild web

--- apps/web/jest-shim.ts
+++ apps/web/jest-shim.ts
@@ -1,6 +1,5 @@
 import { TextDecoder, TextEncoder } from "util";
 
 global.TextEncoder = TextEncoder;
-// global.TextDecoder = TextDecoder;
 // global.ArrayBuffer = ArrayBuffer;
 // global.Uint8Array = Uint8Array;
