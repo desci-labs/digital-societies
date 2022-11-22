d0db51ad4f38bf9803dbbd051a1d24ac04e8de23
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-11-22 20:55:19 +0100

    chore: unit tests for orgs state reducer

--- apps/web/src/services/orgs/reducer.test.ts
+++ apps/web/src/services/orgs/reducer.test.ts
@@ -6,23 +6,28 @@ import reducer, {
   setOrg,
   setOrgs,
 } from "./reducer";
-import { FactoryState, Org, PendingOrg } from "./types";
-
-const orgs: Org[] = [];
+import { FactoryState, Org } from "./types";
+import mockOrgs from "test/data/orgs.json";
 
 const initialState: FactoryState = { data: [], isLoading: true };
 
 describe("Admin reducer", () => {
   let store: Store<FactoryState>;
+  const initialOrgs = mockOrgs.slice(1) as Org[];
+  const delegates = [
+    "0x114EA4c82a0B5d54Ce5697272a2De2e4a14D654C",
+    "0xa408bacA39E3B6504FDd599e22F9c4E6dbE0e27E",
+    "0x25534A5a53b6C363a9187af33421fe939664F5c7",
+  ];
 
   beforeEach(() => {
     store = configureStore({ reducer: reducer });
-    store.dispatch(setOrgs(orgs));
+    store.dispatch(setOrgs(initialOrgs));
   });
 
   describe("setOrgs", () => {
     it("adds orgs to state", () => {
-      expect(store.getState()).toEqual(initialState);
+      expect(store.getState()).toEqual({ ...initialState, data: initialOrgs });
     });
   });
 
@@ -36,39 +41,34 @@ describe("Admin reducer", () => {
 
   describe("setOrg", () => {
     it("Adds new recipient", () => {
-      // store.dispatch(
-      //   setOrg({})
-      // );
+      const org = mockOrgs[0] as Org;
+      store.dispatch(setOrg(org));
       const state = store.getState();
-      // const added = state.find((r) => r.address === "0x5");
-      // expect(added).toEqual(undefined);
+      const added = state.data.find((r) => r.address === org.address);
+      expect(added).toEqual(org);
     });
   });
 
   describe("addDelegate", () => {
     it("Adds new delegate", () => {
-      // store.dispatch(addDelegate());
+      const org = mockOrgs[1];
+      store.dispatch(addDelegate({ org: org.address, delegate: delegates[0] }));
       const state = store.getState();
-      // const added = state.find((r) => r.address === "0x5");
-      // expect(added).toEqual({
-      //   address: "0x5",
-      //   is_added: true,
-      //   is_deleted: false,
-      // });
+      const added = state.data.find((val) => val.address === org.address);
+      expect(added?.delegates).toEqual(org.delegates.concat(delegates[0]));
     });
   });
 
   describe("removeDelegate", () => {
-    beforeEach(() => {
-      // store.dispatch(addDelegate())
-    });
-
     it("Undo addDelegate action", () => {
-      // store.dispatch(removeDelegate());
+      const org = mockOrgs[1];
+      store.dispatch(
+        removeDelegate({ org: org.address, delegate: org.delegates[0] })
+      );
       let state = store.getState();
       state = store.getState();
-      // const undo = state.find((r) => r.address === "0x5");
-      // expect(undo).toEqual(undefined);
+      const added = state.data.find((val) => val.address === org.address);
+      expect(added?.delegates).toEqual(org.delegates.slice(1));
     });
   });
 });
