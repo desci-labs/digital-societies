351341204c39d9d00936710a6e4edf3b36e72f54
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-11-23 11:56:58 +0100

    test: helper functions unit tests

--- apps/web/src/helper/helper.test.ts
+++ apps/web/src/helper/helper.test.ts
@@ -1,5 +1,11 @@
 import { W3S_IPFS_GATEWAY } from "api/constants";
-import { getImageURL, maskAddress, shortenAddress, shortenText } from "helper";
+import {
+  compareMetadata,
+  getImageURL,
+  maskAddress,
+  shortenAddress,
+  shortenText,
+} from "helper";
 
 describe("Helper functions", () => {
   describe("shorten text and address", () => {
@@ -35,7 +41,8 @@ describe("Helper functions", () => {
 
   describe("Resolve Image url", () => {
     const cid = "bafkreiay36go5cuxpoitjjxwlauiyf6rygk5d544z7noapqfwwb3yyuyyy";
-    const w3sImageUrl = "";
+    const w3sImageUrl =
+      "https://w3s.link/ipfs/bafkreid7a4r2gp5py244cjobxpoedhu4sv6cnqktl4hur5rxppnwqkhora";
 
     it("Resolves empty string", () => {
       const result = getImageURL("");
@@ -44,8 +51,37 @@ describe("Helper functions", () => {
 
     it("Resolves CID string", () => {
       const result = getImageURL(cid);
-      console.log(result);
       expect(result).toEqual(`${W3S_IPFS_GATEWAY}${cid}`);
+    });
+
+    it("Resolves w3storage link", () => {
+      const result = getImageURL(w3sImageUrl);
+      expect(result).toEqual(w3sImageUrl);
+    });
+  });
+
+  describe("compare metadata", () => {
+    const metaA = {
+      name: "DeSci Foundation",
+      symbol: "DF",
+      description:
+        "We are building tools to enable scientists to form Autonomous Research Communities (ARCs) that operate on the Ledger of Scientific Record.\n\nARCs select, validate, and curate promising scientific discoveries. Each step of the process is recorded on-chain, creating an immutable and permanent public record.",
+      banner:
+        "https://w3s.link/ipfs/bafkreihn7ci5rurrpvqloqz2s2gosllkz5j2udgcpdo42hquvlmhpvi5km",
+      image:
+        "https://w3s.link/ipfs/bafkreihgkhxi6umjfu34jrkxcqbvumb7lqgdskb6lhenerukalhtespnwm",
+      external_link: "https://desci.com",
+    };
+    const metaAVariant = { ...metaA, image: "" };
+
+    it("Returns false for same metadata object", () => {
+      const result = compareMetadata(metaA, metaA);
+      expect(result).toEqual(false);
+    });
+
+    it("Returns true for different metadata object", () => {
+      const result = compareMetadata(metaA, metaAVariant);
+      expect(result).toEqual(true);
     });
   });
 });
