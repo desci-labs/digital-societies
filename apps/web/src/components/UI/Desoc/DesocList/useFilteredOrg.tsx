1b901299b795fbb0dae0a4b4f1545df04f2084aa
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-11-27 01:24:34 +0100

    feat: filter disabled societies in DesocList

--- apps/web/src/components/UI/Desoc/DesocList/useFilteredOrg.tsx
+++ apps/web/src/components/UI/Desoc/DesocList/useFilteredOrg.tsx
@@ -1,6 +1,7 @@
 import sanitizeRegexSearchText from "helper/sanitizeRegexSearchText";
 import useDebouncer from "hooks/useDebouncer";
 import { useMemo, useState } from "react";
+import { useGetSocietiesQuery } from "services/api/admin";
 import { useGetOrgState } from "services/orgs/hooks";
 
 const DELAY = 250;
@@ -8,11 +9,21 @@ export default function useFilteredOrg() {
   const [searchText, setSearchText] = useState("");
   const { data: orgList, isLoading } = useGetOrgState();
   const [debouncedSearchText, isDebouncing] = useDebouncer(searchText, DELAY);
+  const { data: societies } = useGetSocietiesQuery({}, {});
 
   const filteredList = useMemo(() => {
     const sanitizedText =
       sanitizeRegexSearchText(debouncedSearchText).toLowerCase();
+
     return orgList
+      .filter(
+        (org) =>
+          Array.isArray(societies) &&
+          !societies.find(
+            (society) =>
+              society.address === org.address && society.disabled === true
+          )
+      )
       .filter(
         (org) =>
           org.address.toLowerCase().search(sanitizedText) !== -1 ||
@@ -23,7 +34,7 @@ export default function useFilteredOrg() {
             .search(sanitizedText) !== -1
       )
       .sort((orgA, orgB) => Number(orgB.verified) - Number(orgA.verified));
-  }, [debouncedSearchText, orgList]);
+  }, [debouncedSearchText, orgList, societies]);
 
   function handleSearchTextChange(text: string) {
     setSearchText(text);
