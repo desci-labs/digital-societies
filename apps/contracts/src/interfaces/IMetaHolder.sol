683b65ed280aeaa4677dc55866920eb5a402f652
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-12-05 10:37:16 +0100

    WIP: implement ImetaHolder interface

--- apps/contracts/src/interfaces/IMetaHolder.sol
+++ apps/contracts/src/interfaces/IMetaHolder.sol
@@ -26,10 +26,58 @@ interface IMetaHolder {
     /**
      * @dev Emitted when token `tokenId` is revoked or burned
      */
-    event RevokedToken(uint16 tokenId);
+    event RevokedToken(uint256 tokenId);
 
     /**
      * @dev Emitted when tokens `tokenId` are revoked or burned
      */
     event RevokedTokens(uint16[] tokenIds);
+
+    // setFactoryAddress - only owner
+    /**
+     * @dev set the desoc manager/factory contract address
+     */
+    function setFactoryAddress(address factory) external;
+
+    /**
+     * @dev external function called by factory contract to add a newly deployed society
+     */
+    function addSociety(address society, string calldata uri) external;
+
+    // updateSociety(uri) - isValidSociety
+    /**
+     * @dev external function called by factory contract to add a newly deployed society
+     */
+    function updateSociety(string calldata uri) external;
+
+    // updateAttestation(uint16 type, string uri) -> isValidSociety
+     /**
+     * @dev update attestation token uri
+     */
+    function updateAttestation(uint16 id, string calldata uri) external;
+
+    // addRecipient(uint16 type, address recipient) -> isValidSociety
+     /**
+     * @dev issue a new attestation to `recipient`
+     */
+    function addRecipient(uint16 attestationId, address recipient) external;
+
+    // addRecipients(uint16 type, address[] recipients) -> isValidSociety
+     /**
+     * @dev Issue a new attestation `recipients`
+     */
+    function addRecipients(uint16 attestationId, address[] calldata recipients)
+        external;
+
+    // revokeToken(uint256 tokenId) -> isValidSociety
+     /**
+     * @dev revoke token of Id `tokenId`
+     */
+    function revokeToken(uint256 tokenId) external;
+
+    // revokeToken(uint256 tokenId[]) -> isValidSociety
+     /**
+     * @dev revoke tokens in the array of `tokenIds`
+     */
+    function revokeTokens(uint256[] calldata tokenIds) external;
 }
